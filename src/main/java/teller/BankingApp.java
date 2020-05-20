c494e028692d24492b3ae21269d59530f093a6f7
Nick Naumenko <carswithcharacter614@gmail.com>
2020-05-20 15:08:43 -0400

    May 20 3:09 version of Bank app

--- src/main/java/teller/BankingApp.java
+++ src/main/java/teller/BankingApp.java
@@ -4,7 +4,14 @@ public class BankingApp {
     public static void main(String[] args) {
 
         Bank bank = new Bank ();
-        Account account1 = new Account();
+        Account account1 = new Account("54899", "Checking", 5487.65);
+        Account account2 = new Account ("78457", "Savings", 47894.00);
+
+        bank.openAccount(account1);
+        bank.openAccount(account2);
+
+        System.out.println(bank.getAllAccounts());
+
 
 
     }
